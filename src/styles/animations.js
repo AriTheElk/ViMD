e98c5654892aa3d4aa2da96c3ed04a509c1af09d
aria <aria@elk.wtf>
2016-12-10 12:03:14 -0500

    docs: add doc titles to animations (#1)
    * `fadeIn`
    * `fadeInTop`
    * `fadeInBottom`
    * `fadeInLeft`
    * `fadeInRight`

--- src/styles/animations.js
+++ src/styles/animations.js
@@ -1,5 +1,8 @@
 import { keyframes } from 'styled-components';
 
+/**
+ * CSS animation for fading in without moving
+ */
 export const fadeIn = keyframes`
 0% {
   opacity: 0;
@@ -9,6 +12,9 @@ export const fadeIn = keyframes`
 }
 `;
 
+/**
+ * CSS animation for fading in from the top
+ */
 export const fadeInTop = keyframes`
 0% {
   opacity: 0;
@@ -20,6 +26,9 @@ export const fadeInTop = keyframes`
 }
 `;
 
+/**
+ * CSS animation for fading in from the bottom
+ */
 export const fadeInBottom = keyframes`
 0% {
   opacity: 0;
@@ -31,6 +40,9 @@ export const fadeInBottom = keyframes`
 }
 `;
 
+/**
+ * CSS animation for fading in from the left
+ */
 export const fadeInLeft = keyframes`
 0% {
   opacity: 0;
@@ -42,6 +54,9 @@ export const fadeInLeft = keyframes`
 }
 `;
 
+/**
+ * CSS animation for fading in from the right
+ */
 export const fadeInRight = keyframes`
 0% {
   opacity: 0;
