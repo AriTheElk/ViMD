a5b06197f34d1664a13c991f7a9b27a674686ea8
Garet McKinley <self@mediachicken.com>
2016-12-10 15:16:22 -0500

    fix(git): add missing Icon component (#3)
    For some reason, git was claiming this file was ignored (even though it appears in none of the ignore files). Force-adding it with this commit, brushing it off as a bug.

---
+++ src/components/Icon/Link.js
@@ -0,0 +1,14 @@
+/* @flow */
+import styled from 'styled-components';
+
+import Link from '../Link';
+
+// override the Link component
+export default styled(Link)`
+  color: #fff;
+  font-size: 23px;
+  line-height: 25px;
+  position: relative;
+  top: 3px;
+  margin: 0 5px;
+`;
