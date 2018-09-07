87068d1d7006c52f51cd743458ce1a49b52f80a3
aria <aria@elk.wtf>
2018-09-07 10:49:38 -0500

    fix fadeIn animation on preview component

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -3,11 +3,11 @@ import Grid from "hedron";
 import { fadeIn } from "../../styles/animations";
 
 export default styled(Grid.Box)`
-  animation-delay: 0.25s;
-  animation-timing-function: ease-out;
-  animation: ${fadeIn} 2s normal forwards;
-  max-height: calc(100vh - ${props => props.theme.header.height});
   opacity: 0;
+  animation: ${fadeIn} 2s normal forwards;
+  animation-delay: 0.75s;
+  animation-timing-function: ease-out;
+  max-height: calc(100vh - ${props => props.theme.header.height});
   overflow: auto;
   word-wrap: break-word;
   padding: 5px 25px;
