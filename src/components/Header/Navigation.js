ab357bbfe7a94edd161394217f58d5a354ea0b50
aria <aria@elk.wtf>
2018-09-06 21:17:07 -0500

    make nav animations snappier

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,14 +1,14 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
-import ButtonGroup from '../ButtonGroup';
-import { fadeInTop } from '../../styles/animations';
+import ButtonGroup from "../ButtonGroup";
+import { fadeInTop } from "../../styles/animations";
 
 /**
  * Add a fadeInTop animation to our navigation links
  */
 export default styled(ButtonGroup)`
-  animation: ${fadeInTop} .6s normal forwards;
+  animation: ${fadeInTop} 0.6s normal forwards;
   animation-timing-function: ease-out;
-  animation-delay: 2s;
+  animation-delay: 1s;
   opacity: 0;
 `;
