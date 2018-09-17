e2241eec628af5a930ac83d4b8577588b544b95d
Garet McKinley <self@garet.io>
2018-09-17 01:04:13 -0500

    add LoadingState component

---
+++ src/components/LoadingState/Wrapper.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${props => props.theme.header_background};
+
+  div {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+  }
+  div span {
+    position: absolute;
+    border: 4px solid #fff;
+    opacity: 1;
+    border-radius: 50%;
+    animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+  div span:nth-child(2) {
+    animation-delay: -0.5s;
+  }
+
+  @keyframes lds-ripple {
+    0% {
+      top: 28px;
+      left: 28px;
+      width: 0;
+      height: 0;
+      opacity: 1;
+    }
+    100% {
+      top: -1px;
+      left: -1px;
+      width: 58px;
+      height: 58px;
+      opacity: 0;
+    }
+  }
+`;
+
+export default Wrapper;
---
+++ src/components/LoadingState/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Wrapper from "./Wrapper";
+
+const LoadingState = () => (
+  <Wrapper>
+    <div>
+      <span />
+      <span />
+    </div>
+  </Wrapper>
+);
+
+export default LoadingState;
