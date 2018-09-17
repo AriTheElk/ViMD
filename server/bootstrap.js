6b44411374fd8eb4e54e37c39619caba13e5c286
Garet McKinley <self@garet.io>
2018-09-16 23:53:16 -0500

    add polyfill for localStorage

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,12 +2,16 @@ require("ignore-styles");
 
 require("babel-register")({
   ignore: [/(node_modules)/],
-  presets: ["es2015", "react-app"],
+  presets: ["es2015", "react-app"]
 });
 
 // TODO: Figure out a better way to polyfill window in node
 if (typeof window === "undefined") {
   global.window = {};
+}
+
+if (typeof localStorage === "undefined") {
+  global.localStorage = { getItem: () => {} };
 }
 
 require("./index");
